fd11c468cbd74f27f7eca60833eb6da9ecc91b29
夏雷 <892095114@qq.com>
2018-11-06 10:26:04 +0800

    提交java项目

---
+++ testGit/src/testgit/TestGit.java
@@ -0,0 +1,16 @@
+package testgit;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("你好,世界");
+        System.out.println("你好,世界");
+        System.out.println("你好,世界");
+        System.out.println("你好,世界");
+        System.out.println("你好,世界");
+        System.out.println("你好,世界");
+        System.out.println("你好,世界");
+        System.out.println("你好,世界");
+    }
+}
